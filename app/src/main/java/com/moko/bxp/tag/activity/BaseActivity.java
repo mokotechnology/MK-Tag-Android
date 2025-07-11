30fce84a7d11e305d76de77066115d6d8fd7614e
liuwenzheng <329541594@qq.com>
2025-07-11 17:16:15 +0800

    编译版本改为35，解决UI兼容问题

--- app/src/main/java/com/moko/bxp/tag/activity/BaseActivity.java
+++ app/src/main/java/com/moko/bxp/tag/activity/BaseActivity.java
@@ -4,10 +4,14 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.view.DisplayCutout;
 
 import com.elvishew.xlog.XLog;
+
+import java.util.List;
 
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
@@ -26,6 +30,17 @@ public abstract class BaseActivity<VM extends ViewBinding> extends FragmentActiv
             startActivity(intent);
             return;
         }
+        getWindow().getDecorView().setOnApplyWindowInsetsListener((v, insets) -> {
+            DisplayCutout cutout = insets.getDisplayCutout();
+            if (cutout != null) {
+                List<Rect> rects = cutout.getBoundingRects();
+                if (rects.size() != 0) {
+                    getWindow().getDecorView().setPadding(cutout.getSafeInsetLeft(), cutout.getSafeInsetTop(),
+                            cutout.getSafeInsetRight(), cutout.getSafeInsetBottom());
+                }
+            }
+            return insets;
+        });
         mBind = getViewBinding();
         setContentView(mBind.getRoot());
         onCreate();
